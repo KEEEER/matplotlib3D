a2c98e1b07c425979f86a2cd3450bce1e958310c
KEEEER <chris5454561@gmail.com>
2018-12-28 17:30:49 +0800

    daily

--- matplot3D.py
+++ matplot3D.py
@@ -4,14 +4,22 @@ import numpy as np
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-with open("the-file-name.txt") as f:
-    for line in f:
-        if ',' in line:
-	        r, f , m = line.split(",", 2)
-	        R = float(r)
-	        F = float(f)
-	        M = float(m)
-	        ax.scatter(R, F, M, c='k', marker='.')                 
+color = ['b','g','r','c','m']
+R=[]
+F=[]
+M=[]
+C=[]
+if not R:
+	with open("the-file-name.txt") as f:
+	    for line in f:
+	        if ',' in line:
+		        r, f , m , c= line.split(",", 3)
+		        R.append(float(r))
+		        F.append(float(f))
+		        M.append(float(m))
+		        C.append(color[int(c)])
+		        
+ax.scatter(R, F, M, c=C, marker='.')	                         
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
